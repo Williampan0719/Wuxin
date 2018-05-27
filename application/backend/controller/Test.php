e98e5bd9e58db72ff92163c8a52b0e9f626c2e92
panhao <panhao@pgyxwd.com>
2018-05-27 15:18:39 +0800

    test

--- application/backend/controller/Test.php
+++ application/backend/controller/Test.php
@@ -8,18 +8,8 @@
 
 namespace app\backend\controller;
 
-
-use app\backend\logic\TradingLogic;
-use app\backend\model\AdminRole;
-use extend\helper\Curl;
-use extend\helper\Utils;
-use think\Loader;
+use think\Db;
 use think\Request;
-
-Loader::import('thirdpart.aop.AopClient');
-Loader::import('thirdpart.aop.request.ZhimaCreditScoreBriefGetRequest');
-//require_once __DIR__.'/../../../extend/thirdpart/aop/AopClient.php';
-//require_once __DIR__.'/../../../extend/thirdpart/aop/request/ZhimaCreditScoreBriefGetRequest.php';
 
 class Test extends BaseAdmin
 {
@@ -29,23 +19,6 @@ class Test extends BaseAdmin
     }
 
     public function index(){
-        $param = $this->param;
-        $new = new TradingLogic();
-        $list = $new->BuyWater($param);
-    }
-
-    public function testTd(){
-        $url = 'https://api.tongdun.cn/bodyguard/apply/v4.1?partner_code=xrqb&partner_key=d422ae7d6daa41fa808c59422fb31f17&app_name=pgyqb_web';
-        $param = $this->request->param();
-        $data['id_number'] = $param['idcard'];
-        $data['account_name'] = $param['name'];
-        $data['biz_code'] = 'pgysmrzweb';
-        $result = Curl::postUrlencoded($url, $data,true);
-        dump($result);exit;
-    }
-
-    public function roleTest()
-    {
-
+        return Db::table('wx_attendance')->group('uuid')->field('uuid,SUM(ask_leave) as ask_leave,SUM(absent) as absent')->select();
     }
 }
