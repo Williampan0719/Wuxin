085ed118dddab7d40a73199b910c4c8366d42555
panhao <panhao@pgyxwd.com>
2018-05-30 19:46:28 +0800

    fix

--- public/index.php
+++ public/index.php
@@ -15,7 +15,6 @@
 define('APP_PATH', __DIR__ . '/../application/');
 //加载配置文件路径
 define('CONF_PATH', __DIR__.'/../config/');
-dump(2222);exit;
 
 // 加载框架引导文件
 require __DIR__ . '/../thinkphp/start.php';
